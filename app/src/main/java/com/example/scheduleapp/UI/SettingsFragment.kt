b1a7e5aeda36b8393faa7b0037c12491a1e7c5d8
NinetyTwo-TwentyNine <yanshchpy@gmail.com>
2023-03-29 19:40:29 +0300

    Added an option to log out (settings fragment).

--- app/src/main/java/com/example/scheduleapp/UI/SettingsFragment.kt
+++ app/src/main/java/com/example/scheduleapp/UI/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.scheduleapp.UI
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,14 +8,22 @@ import android.view.ViewGroup
 import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.scheduleapp.R
+import com.example.scheduleapp.databinding.FragmentLoginBinding
+import com.example.scheduleapp.databinding.FragmentSettingsBinding
 import com.example.scheduleapp.viewmodels.MainActivityViewModel
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SettingsFragment : Fragment() {
     private val exampleViewModel: MainActivityViewModel by activityViewModels()
+
+    lateinit var mAuth: FirebaseAuth
+    lateinit var mPreferences: SharedPreferences
+    private lateinit var binding: FragmentSettingsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,18 +33,31 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        mAuth = (activity as MainActivity).mAuth
+        mPreferences = (activity as MainActivity).mPreferences
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<SwitchCompat>(R.id.enablePushesCheckBox).setOnCheckedChangeListener(){v, checked ->
+        binding.enablePushesCheckBox.setOnCheckedChangeListener(){v, checked ->
             if(checked)
             {
                 findNavController().navigate(R.id.loginFragment)
             }
+        }
+
+        binding.logoutTrigger.setOnClickListener {
+            mAuth.signOut()
+            mPreferences.edit()
+                .putBoolean("APP_PREFERENCES_STAY", false)
+                .apply()
+            view.findNavController()
+                .navigate(FragmentContainerDirections.actionFragmentContainerToLoginFragment())
         }
     }
 
